6b9614a77cb74c895356b938f7d7a4c429c09c7c
Dynesshely <catrol@qq.com>
2022-04-24 03:33:20 +0800

    💾 Feat: 我学会了哈哈哈!

---
+++ WpfApp1/Commands/DelegateCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => CanExecuteFunc == null || CanExecuteFunc(parameter);
+
+        public void Execute(object parameter) => ExecuteAction(parameter);
+
+        public Action<object> ExecuteAction { get; set; }
+
+        public Func<object, bool> CanExecuteFunc { get; set; }
+    }
+}
---
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Commands;
+
+namespace WpfApp1.ViewModels
+{
+    internal class MainWindowViewModel : NotificationObject
+    {
+        private double input1;
+
+        public double Input1
+        {
+            get { return input1; }
+            set
+            {
+                input1 = value;
+                RaisePropertyChanged(nameof(Input1));
+            }
+        }
+
+        private double input2;
+
+        public double Input2
+        {
+            get { return input2; }
+            set
+            {
+                input2 = value;
+                RaisePropertyChanged(nameof(Input2));
+            }
+        }
+
+        private double result;
+
+        public double Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                RaisePropertyChanged(nameof(Result));
+            }
+        }
+
+        public DelegateCommand? AddCommand { get; set; }
+        public DelegateCommand? SaveCommand { get; set; }
+
+        private void Add(object _)
+        {
+            Result = Input1 + Input2;
+        }
+
+        private void Save(object _)
+        {
+            SaveFileDialog sfd = new();
+            sfd.ShowDialog();
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new();
+            AddCommand.ExecuteAction = new(Add);
+            SaveCommand = new();
+            SaveCommand.ExecuteAction = new(Save);
+        }
+
+
+    }
+}
